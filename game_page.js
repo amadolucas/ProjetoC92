0ccf184ac628fb9d8b2dfbe1520af5bc4a1a6f3a
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-12-22 14:57:35 -0300

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -22,7 +22,7 @@ function send() {
 
 
     question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
-    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Verificar</button>";
     row =  question_number + input_box + check_button ; 
     document.getElementById("output").innerHTML = row;
